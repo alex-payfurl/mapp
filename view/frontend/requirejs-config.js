873232382df302607c8edc88e194f605b92cc280
Alex Ovseyenko <alex@payfurl.com>
2023-06-30 12:27:42 +1000

    initial commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+let config = {
+    map: {
+        '*': {
+            'payfurljs': 'https://assets.payfurl.com/v4.1.1/js/payfurl.js',
+        }
+    }
+};
